6ec0ad11ae34e4b49abec82193b36fb809d49c13
tomzdev <tomzxd.yt@gmail.com>
2023-06-12 12:53:05 +0200

    primo blog

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,32 @@
 import Head from 'next/head'
 
-const post = [
+const posts = [
   {title: 'React Testing', excerpt: 'Learn react testing'},
   {title: 'React with Tailwind', excerpt: 'Learn react with tailwind'},
 ];
 
 export default function Home() {
   return (
-    <div className = "container mx-auto px-10 mb-8">
+    <main className = "container mx-auto px-10 mb-8">
       <Head>
-        <title>Blog di Tomz</title>
+        <title>Tomz Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className = "grid grid-cols-1 lg:grid-cols-12 gap-12">
-        {post.map(post => (post,index) => (
-          <div>
-            {post.title}
-            {post.excerpt}
-          </div>
+        <div className='lg:col-span-8 col-span-1'>
+          {posts.map((post, index) => (
+            <div>
+              {post.title}
+              {post.excerpt}
+            </div>
           ))}
+        </div>
+        <div className='lg:col-span-4 COL-SPA'>
+          <div className='lg:sticky relative top-8'>
+            
+          </div>
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
