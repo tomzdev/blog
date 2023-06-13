9b68826054d98093528abc604137afa503687ffa
tomzdev <tomzxd.yt@gmail.com>
2023-06-13 12:14:01 +0200

    funzionaa

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -11,7 +11,14 @@ const FeaturedPostCard = ({ post }) => (
       <p className="text-white mb-4 text-shadow font-semibold text-xs">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
       <p className="text-white mb-4 text-shadow font-semibold text-2xl text-center">{post.title}</p>
       <div className="flex items-center absolute bottom-5 w-full justify-center">
-
+        <img
+          unoptimized
+          alt={post.author.name}
+          height="30px"
+          width="30px"
+          className="align-middle drop-shadow-lg rounded-full"
+          src={post.author.photo.url}
+        />
         <p className="inline align-middle text-white text-shadow ml-2 font-medium">{post.author.name}</p>
       </div>
     </div>
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -27,7 +27,15 @@ const PostWidget = ({ categories, slug }) => {
       {relatedPosts.map((post, index) => (
         <div key={index} className="flex items-center w-full mb-4">
           <div className="w-16 flex-none">
-
+            <img
+              loader={grpahCMSImageLoader}
+              alt={post.title}
+              height="60px"
+              width="60px"
+              unoptimized
+              className="align-middle rounded-full"
+              src={post.featuredImage.url}
+            />
           </div>
           <div className="flex-grow ml-4">
             <p className="text-gray-500 font-xs">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
